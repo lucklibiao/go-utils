ef99ad2354a8b4c9cdee57c8206948b438619e58
unknown <libiao@sinosafe.local>
2021-03-05 16:48:31 +0800

    add log

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # golang
-golang utils
+golang utils include :
+1、 log 用于写日志，写日志包括标准输出以及指定输出到指定的日志文件中，计划完成时间3月底，采用的方式主要借鉴GOLANG提供的标准日志输出
+2、
+3、
+4、
+5、
+6、
+7、
+
---
+++ log/glog.go
@@ -0,0 +1,32 @@
+package log
+
+import "sync/atomic"
+
+type severity int32 //定义日志级别
+
+//log level from 0 to 3
+// high-severity is also writtten to each loger-severity log file
+const (
+	infoLog severity =iota
+	waringLog
+	errorLog
+	fatalLog
+	numSeverity = 4
+)
+
+const severityChar="IWEF" // info waring error fatal
+
+// get returns the value of severity,add atomic to protect the level
+func (s *severity) get() severity{
+	return severity(atomic.LoadInt32((*int32)(s)))
+}
+
+func (s *severity) set(level severity){
+	atomic.StoreInt32((*int32)(s),int32(level))
+
+}
+func (s *severity) String() string{
+
+}
+
+
